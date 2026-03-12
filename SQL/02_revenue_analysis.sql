ccb8d63c74723fc95ea9001801ae77431e54e7e1
Minh Dao <minh.dao@kyanon.digital>
2026-03-12 22:04:16 +0700

    update - add more revenue-percent column into top 10 products for insight

--- SQL/02_revenue_analysis.sql
+++ SQL/02_revenue_analysis.sql
@@ -83,14 +83,22 @@ ORDER BY total_revenue DESC
 LIMIT 5;
 -- --------------------------------------------------------
 -- Top 10 Products by Revenue
-SELECT i.product_id,
-	SUM(i.price + i.freight_value) AS product_revenue
-FROM olist_orders_dataset o
-JOIN olist_order_items_dataset i
-	ON o.order_id = i.order_id
-WHERE o.order_status NOT IN ('canceled', 'unavailable')
-GROUP BY i.product_id
-ORDER BY product_revenue DESC
+WITH product_revenue AS (
+	SELECT i.product_id,
+		   SUM(i.price + i.freight_value) AS revenue
+	FROM olist_orders_dataset o
+	JOIN olist_order_items_dataset i
+		ON o.order_id = i.order_id
+	WHERE o.order_status NOT IN ('canceled', 'unavailable')
+	GROUP BY i.product_id
+)
+
+SELECT 
+	product_id,
+	revenue,
+	ROUND(revenue * 100.0 / (SELECT SUM(revenue) FROM product_revenue), 2) AS revenue_percent
+FROM product_revenue
+ORDER BY revenue DESC
 LIMIT 10;
 -- --------------------------------------------------------------------------------
 -- Top 10 Sellers by Revenue
